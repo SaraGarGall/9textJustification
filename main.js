67c38d67cd05c35830fcaafd4a2c9b950badfe49
SaraGarGall <saragarciagallardo@gmail.com>
2024-10-06 18:17:44 +0100

    main.js structure created. .jason created. function justificationWithSpaces in utility.js created

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-/*9)	Text Justification: Write a program that justifies a block of text to a specific width by adding spaces between words (similar to word wrap functionality).*/
+/*9. Text Justification: Write a program that justifies a block of text to a specific width by adding spaces between words (similar to word wrap functionality).*/
+
+import { justificationWithSpaces } from './utility.js';
+import { inquirer} from 'inquirer';
+
+let blockOfText = await inquirer.prompt([{
+        type: "input",
+        name:  "input",
+        message: "type the block of text you want to justify:"
+    }
+]);
+
+const result = justificationWithSpaces(blockOfText.input);
+
+console.log("Your block of text justified is:", result);
+
---
+++ utility.js
@@ -0,0 +1,13 @@
+export function justificationWithSpaces(n){
+    let text = n;
+    let width = 30;
+    console.log('text.length', text.length);
+
+    if(text.length < width){
+        for(let i= 0; i < text.length; i++){
+            //to add an space after this letter
+        }
+    } 
+    
+    return text;
+}

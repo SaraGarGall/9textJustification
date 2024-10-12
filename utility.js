fd83dcebd0ecf1254465b400485e72ce8e315341
SaraGarGall <saragarciagallardo@gmail.com>
2024-10-12 13:54:59 +0100

    change done as a test

--- utility.js
+++ utility.js
@@ -20,16 +20,18 @@ export function justificationWithSpaces(n){
 
 //NO ESTOY CONVENCIDA DEL FOR LOOP
     for(let i = 0; i < text.length; i++){
-        if(string.length < width){
+        if(string.length <= width){
             for(let j = 0; j < (text.length - 1); j++){
                 //añadir un espacio despues de cada elemento del array, de cada palabra
                 text[j]=text[j] + " ";
                 string = text.join(' '); //actualizo el string con los cambios en el array de text.
                 console.log("text inside 2nd for loop:", text);
+                
             }
         }
     }
     string = text.join(' ');
     console.log('string length:', string.length);
     return string;// Para devolver un string y no un array.
+    //PROBLEMA: devuelve un string mas corta que el width
 }
